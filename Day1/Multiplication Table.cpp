d5d7e933c92af889e2c01fcc24fef5f4910e2aeb
Shikhar <shikharsks28@gmail.com>
2026-06-01 20:13:27 +0530

    Update Multiplication Table.cpp

--- Day1/Multiplication Table.cpp
+++ Day1/Multiplication Table.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"Enter number: ";
+    cout<<"Enter the number: ";
     cin>>n;
     for(int i=1;i<=10;i++){
         cout<<n<<" x "<<i<<" = "<<n*i<<endl;
