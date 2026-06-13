c997da16c3f2cd95d7fb95b86a0f6b49a787db70
Shikhar <shikharsks28@gmail.com>
2026-06-13 20:51:33 +0530

    Fix formatting and syntax in Input and Display Array.cpp

--- Day13/Input and Display Array.cpp
+++ Day13/Input and Display Array.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
+
 using namespace std;
-int main(){
+
+int main()
+{
     int n;
     cout<<"Enter size: ";
     cin>>n;
+    
     int a[100];
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++)
+    {
         cout<<"Enter element "<<i+1<<": ";
         cin>>a[i];
     }
     cout<<"Array: ";
-    for(int i=0;i<n;i++) cout<<a[i]<<" ";
+    for(int i=0;i<n;i++)
+    cout<<a[i]<<" ";
     cout<<endl;
+    
     return 0;
 }
