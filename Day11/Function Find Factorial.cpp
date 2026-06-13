8a178f510fbe88bf281bbf6fe2ffea72074dd47e
Shikhar <shikharsks28@gmail.com>
2026-06-13 11:45:41 +0530

    Update Function Find Factorial.cpp

--- Day11/Function Find Factorial.cpp
+++ Day11/Function Find Factorial.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
-long long factorial(int n){
-    long long fact=1;
+int factorial(int n){
+    int fact=1;
     for(int i=1;i<=n;i++) fact=fact*i;
     return fact;
 }
