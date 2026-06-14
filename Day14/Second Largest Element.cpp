c826b90653f3a1786d3f3d804f0ed6915cec3fcd
Shikhar <shikharsks28@gmail.com>
2026-06-14 18:11:05 +0530

    Update Second Largest Element.cpp

--- Day14/Second Largest Element.cpp
+++ Day14/Second Largest Element.cpp
@@ -1,15 +1,24 @@
 #include<iostream>
+
 using namespace std;
+
 int main(){
+    
     int n, a[100];
     cout<<"Enter size: ";
     cin>>n;
-    for(int i=0;i<n;i++) cin>>a[i];
+    
+    for(int i=0;i<n;i++)
+        cin>>a[i];
     int largest=a[0], second=a[0];
-    for(int i=1;i<n;i++){
-        if(a[i]>largest){ second=largest; largest=a[i]; }
-        else if(a[i]>second && a[i]!=largest) second=a[i];
+    for(int i=1;i<n;i++)
+    {
+        if(a[i]>largest)
+        { second=largest; largest=a[i]; }
+        else if(a[i]>second && a[i]!=largest) 
+            second=a[i];
     }
     cout<<"Second largest = "<<second<<endl;
+    
     return 0;
 }
