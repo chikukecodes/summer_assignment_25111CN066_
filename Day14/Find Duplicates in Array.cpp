8f2b48a065b9aeda78ee93fb61aca2f1014e52c9
Shikhar <shikharsks28@gmail.com>
2026-06-14 18:08:20 +0530

    Update Find Duplicates in Array.cpp

--- Day14/Find Duplicates in Array.cpp
+++ Day14/Find Duplicates in Array.cpp
@@ -1,16 +1,26 @@
 #include<iostream>
 using namespace std;
-int main(){
+
+int main()
+{
     int n, a[100];
-    cout<<"Enter size: ";
+    
+    cout<<"Enter the size: ";
     cin>>n;
+    
     for(int i=0;i<n;i++) cin>>a[i];
+    
     cout<<"Duplicates: ";
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(a[i]==a[j]){ cout<<a[i]<<" "; break; }
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            { cout<<a[i]<<" "; break; }
         }
     }
     cout<<endl;
+    
     return 0;
 }
