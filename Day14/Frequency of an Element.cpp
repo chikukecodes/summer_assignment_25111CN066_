9052b2bbee453929332c4a860f7bfdf847f22263
Shikhar <shikharsks28@gmail.com>
2026-06-14 18:09:28 +0530

    Update Frequency of an Element.cpp

--- Day14/Frequency of an Element.cpp
+++ Day14/Frequency of an Element.cpp
@@ -1,15 +1,25 @@
 #include<iostream>
+
 using namespace std;
-int main(){
+
+int main()
+{
     int n, a[100], key, count=0;
+    
     cout<<"Enter size: ";
     cin>>n;
+    
     for(int i=0;i<n;i++) cin>>a[i];
+    
     cout<<"Enter key: ";
     cin>>key;
-    for(int i=0;i<n;i++){
-        if(a[i]==key) count++;
+    
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==key)
+            count++;
     }
     cout<<"Frequency = "<<count<<endl;
+    
     return 0;
 }
