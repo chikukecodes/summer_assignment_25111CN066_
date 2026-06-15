6ca99882fce682954285728bc0ae7567d3b4693e
Shikhar <shikharsks28@gmail.com>
2026-06-15 17:33:49 +0530

    Update Move Zeroes to End.cpp

--- Day15/Move Zeroes to End.cpp
+++ Day15/Move Zeroes to End.cpp
@@ -1,17 +1,29 @@
 #include<iostream>
 using namespace std;
-int main(){
+
+int main()
+{
     int n, a[100];
-    cout<<"Enter size: ";
+    
+    cout<<"Enter no of elements : ";
     cin>>n;
+    
     for(int i=0;i<n;i++) cin>>a[i];
     int pos=0;
-    for(int i=0;i<n;i++){
-        if(a[i]!=0){ a[pos]=a[i]; pos++; }
+    
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]!=0)
+        { a[pos]=a[i]; 
+         pos++; }
     }
-    while(pos<n){ a[pos]=0; pos++; }
+    while(pos<n)
+    { a[pos]=0; 
+     pos++; }
     cout<<"Array: ";
-    for(int i=0;i<n;i++) cout<<a[i]<<" ";
-    cout<<endl;
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+    
+    cout<<"\n";
     return 0;
 }
