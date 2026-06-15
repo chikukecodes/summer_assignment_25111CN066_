fb482603ed398c7f82a4dbb7e77aa97cb989c5e4
Shikhar <shikharsks28@gmail.com>
2026-06-15 17:40:27 +0530

    Refactor input/output prompts and formatting

--- Day15/Rotate Array Right.cpp
+++ Day15/Rotate Array Right.cpp
@@ -1,19 +1,26 @@
 #include<iostream>
+
 using namespace std;
+
 int main(){
+    
     int n, a[100], k;
-    cout<<"Enter size: ";
+    cout<<"Enter no of elements: ";
     cin>>n;
-    for(int i=0;i<n;i++) cin>>a[i];
+    
+    for(int i=0;i<n;i++) 
+        cin>>a[i];
     cout<<"Enter positions to rotate: ";
     cin>>k;
     for(int r=0;r<k;r++){
         int temp=a[n-1];
-        for(int i=n-1;i>0;i--) a[i]=a[i-1];
+        for(int i=n-1;i>0;i--) 
+            a[i]=a[i-1];
         a[0]=temp;
     }
     cout<<"After right rotation: ";
-    for(int i=0;i<n;i++) cout<<a[i]<<" ";
-    cout<<endl;
+    for(int i=0;i<n;i++) 
+        cout<<a[i]<<" ";
+    cout<<"\n";
     return 0;
 }
