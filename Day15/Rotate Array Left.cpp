0312e75a83ba93de8634931fe3a9bb74190767f7
Shikhar <shikharsks28@gmail.com>
2026-06-15 17:39:28 +0530

    Update Rotate Array Left.cpp

--- Day15/Rotate Array Left.cpp
+++ Day15/Rotate Array Left.cpp
@@ -1,19 +1,27 @@
 #include<iostream>
+
 using namespace std;
-int main(){
+int main()
+{
     int n, a[100], k;
-    cout<<"Enter size: ";
+    
+    cout<<"Enter the number of elements: ";
     cin>>n;
-    for(int i=0;i<n;i++) cin>>a[i];
+    for(int i=0;i<n;i++) 
+        cin>>a[i];
     cout<<"Enter positions to rotate: ";
     cin>>k;
+    
     for(int r=0;r<k;r++){
         int temp=a[0];
-        for(int i=0;i<n-1;i++) a[i]=a[i+1];
+        for(int i=0;i<n-1;i++)
+            a[i]=a[i+1];
         a[n-1]=temp;
     }
-    cout<<"After left rotation: ";
-    for(int i=0;i<n;i++) cout<<a[i]<<" ";
-    cout<<endl;
+    cout<<"After the left rotation: ";
+    
+    for(int i=0;i<n;i++) 
+        cout<<a[i]<<" ";
+    cout<<"\n";
     return 0;
 }
