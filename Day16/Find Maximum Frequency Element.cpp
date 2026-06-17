e573f75c2e36f6259ca5d0444dcc71c6de7a6aa2
Shikhar <shikharsks28@gmail.com>
2026-06-17 15:27:59 +0530

    Refactor maximum frequency element program

--- Day16/Find Maximum Frequency Element.cpp
+++ Day16/Find Maximum Frequency Element.cpp
@@ -1,18 +1,28 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n, a[100];
-    cout<<"Enter size: ";
+    int n;
+    
+    cout<<"Enter the no of elements: ";
     cin>>n;
-    for(int i=0;i<n;i++) cin>>a[i];
+
+    int a[n];
+    
+    for(int i=0;i<n;i++) 
+        cin>>a[i];
     int maxFreq=0, element=a[0];
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++)
+    {
         int count=0;
-        for(int j=0;j<n;j++){
-            if(a[j]==a[i]) count++;
+        for(int j=0;j<n;j++)
+        {
+            if(a[j]==a[i]) 
+                count++;
         }
-        if(count>maxFreq){ maxFreq=count; element=a[i]; }
+        if(count>maxFreq)
+        { maxFreq=count; 
+         element=a[i]; }
     }
-    cout<<"Max frequency element = "<<element<<" (appears "<<maxFreq<<" times)"<<endl;
+    cout<<"Max frequency element = "<<element<<" (appears "<<maxFreq<<" times)"<</n";
     return 0;
 }
