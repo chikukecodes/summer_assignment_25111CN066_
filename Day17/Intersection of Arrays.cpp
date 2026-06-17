b77a4e44c00263a2af024952c4f10473c0268c2a
Shikhar <shikharsks28@gmail.com>
2026-06-17 20:48:45 +0530

    Update Intersection of Arrays.cpp

--- Day17/Intersection of Arrays.cpp
+++ Day17/Intersection of Arrays.cpp
@@ -1,19 +1,28 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int n, m, a[100], b[100];
-    cout<<"Enter size of first array: ";
+
+int main()
+{
+    int n, m, a[100], b[100]; 
+    
+    cout<<"Enter the elements of first array: ";
     cin>>n;
-    for(int i=0;i<n;i++) cin>>a[i];
-    cout<<"Enter size of second array: ";
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    cout<<"Enter the elements of second array: ";
     cin>>m;
-    for(int i=0;i<m;i++) cin>>b[i];
+    for(int i=0;i<m;i++)
+        cin>>b[i];
     cout<<"Intersection: ";
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(a[i]==b[j]){ cout<<a[i]<<" "; break; }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(a[i]==b[j])
+            { cout<<a[i]<<" "; 
+             break; }
         }
     }
-    cout<<endl;
+    cout<<"\n";
     return 0;
 }
