eb4a7f0a9d9a704dc69b699aaa74a2f69a394edf
Shikhar <shikharsks28@gmail.com>
2026-06-17 15:29:41 +0530

    Update Find Missing Number in Array.cpp

--- Day16/Find Missing Number in Array.cpp
+++ Day16/Find Missing Number in Array.cpp
@@ -1,12 +1,21 @@
 #include<iostream>
 using namespace std;
-int main(){
+
+int main()
+{
     int n, a[100];
+    
     cout<<"Enter size (numbers from 1 to n+1): ";
+    
     cin>>n;
-    for(int i=0;i<n;i++) cin>>a[i];
+    
+    for(int i=0;i<n;i++) 
+        cin>>a[i];
     int total=(n+1)*(n+2)/2, sum=0;
-    for(int i=0;i<n;i++) sum=sum+a[i];
-    cout<<"Missing number = "<<total-sum<<endl;
+    
+    for(int i=0;i<n;i++) {
+        sum=sum+a[i];
+    }
+    cout<<"Missing number = "<<total-sum<<"\n";
     return 0;
 }
