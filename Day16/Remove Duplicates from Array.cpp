0a0fd2a90d91124c168ed0764cea322df165d4fd
Shikhar <shikharsks28@gmail.com>
2026-06-17 15:34:11 +0530

    Update Remove Duplicates from Array.cpp

--- Day16/Remove Duplicates from Array.cpp
+++ Day16/Remove Duplicates from Array.cpp
@@ -1,19 +1,28 @@
 #include<iostream>
 using namespace std;
+
 int main(){
     int n, a[100], b[100], size=0;
-    cout<<"Enter size: ";
+    cout<<"Enter the elements: ";
     cin>>n;
-    for(int i=0;i<n;i++) cin>>a[i];
+    for(int i=0;i<n;i++) 
+        cin>>a[i];
     for(int i=0;i<n;i++){
         int found=0;
         for(int j=0;j<size;j++){
-            if(b[j]==a[i]){ found=1; break; }
+            if(b[j]==a[i])
+            { found=1;
+             break; }
         }
-        if(found==0){ b[size]=a[i]; size++; }
+        if(found==0)
+        { b[size]=a[i]; 
+         size++; }
     }
     cout<<"Array without duplicates: ";
-    for(int i=0;i<size;i++) cout<<b[i]<<" ";
-    cout<<endl;
+    
+    for(int i=0;i<size;i++) 
+        cout<<b[i]<<" ";
+    
+    cout<<"\n";
     return 0;
 }
