97a4d3ecdbfc44c018bec7dd0a78a717dbd2ca57
Shikhar <shikharsks28@gmail.com>
2026-06-17 15:32:10 +0530

    Update Find Pair with Given Sum.cpp

--- Day16/Find Pair with Given Sum.cpp
+++ Day16/Find Pair with Given Sum.cpp
@@ -1,21 +1,26 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n, a[100], target;
-    cout<<"Enter size: ";
+    int n, a[100], 
+    target;
+    cout<<"Enter no of elemnts: ";
     cin>>n;
-    for(int i=0;i<n;i++) cin>>a[i];
-    cout<<"Enter target sum: ";
+    for(int i=0;i<n;i++) 
+        cin>>a[i];
+    cout<<"Enter the target sum : ";
     cin>>target;
     int found=0;
+    
     for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
+        for(int j=i+1;j<n;j++)
+        {
             if(a[i]+a[j]==target){
-                cout<<"Pair: "<<a[i]<<" + "<<a[j]<<endl;
+                cout<<"Pair: "<<a[i]<<" + "<<a[j]<<"\n";
                 found=1;
             }
         }
     }
-    if(found==0) cout<<"No pair found"<<endl;
+    if(found==0) 
+        cout<<"No pair found"<<"\n";
     return 0;
 }
