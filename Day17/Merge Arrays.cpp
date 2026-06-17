0a37e3ff0a62f23c31bc2bc3fffd0772b71edd62
Shikhar <shikharsks28@gmail.com>
2026-06-17 20:50:06 +0530

    Update Merge Arrays.cpp

--- Day17/Merge Arrays.cpp
+++ Day17/Merge Arrays.cpp
@@ -1,17 +1,23 @@
 #include<iostream>
 using namespace std;
-int main(){
+int main()
+{
     int n, m, a[100], b[100], c[200];
-    cout<<"Enter size of first array: ";
+    
+    cout<<"Enter the elements of first array: ";
     cin>>n;
     for(int i=0;i<n;i++) cin>>a[i];
-    cout<<"Enter size of second array: ";
+    cout<<"Enter the elements of second array: ";
     cin>>m;
-    for(int i=0;i<m;i++) cin>>b[i];
-    for(int i=0;i<n;i++) c[i]=a[i];
-    for(int i=0;i<m;i++) c[n+i]=b[i];
+    for(int i=0;i<m;i++) 
+        cin>>b[i];
+    for(int i=0;i<n;i++) 
+        c[i]=a[i];
+    for(int i=0;i<m;i++) 
+        c[n+i]=b[i];
     cout<<"Merged array: ";
-    for(int i=0;i<n+m;i++) cout<<c[i]<<" ";
-    cout<<endl;
+    for(int i=0;i<n+m;i++) 
+        cout<<c[i]<<" ";
+    cout<<"\n";
     return 0;
 }
