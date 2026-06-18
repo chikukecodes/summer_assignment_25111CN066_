558ab43fed7c8aef91e98d6f6de34ae534c1598a
Shikhar <shikharsks28@gmail.com>
2026-06-18 17:18:07 +0530

    Update Sort Array in Descending Order.cpp

--- Day18/Sort Array in Descending Order.cpp
+++ Day18/Sort Array in Descending Order.cpp
@@ -4,14 +4,19 @@ int main(){
     int n, a[100], temp;
     cout<<"Enter size: ";
     cin>>n;
-    for(int i=0;i<n;i++) cin>>a[i];
+    for(int i=0;i<n;i++) 
+        cin>>a[i];
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-1-i;j++){
-            if(a[j]<a[j+1]){ temp=a[j]; a[j]=a[j+1]; a[j+1]=temp; }
+            if(a[j]<a[j+1])
+            { temp=a[j];
+             a[j]=a[j+1]; 
+             a[j+1]=temp; }
         }
     }
     cout<<"Sorted descending: ";
-    for(int i=0;i<n;i++) cout<<a[i]<<" ";
+    for(int i=0;i<n;i++) 
+        cout<<a[i]<<" ";
     cout<<endl;
     return 0;
 }
