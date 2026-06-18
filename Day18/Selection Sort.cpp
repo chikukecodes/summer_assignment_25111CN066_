76b9b6eb746fe2f074fd6ab6a3c3704673ae5d99
Shikhar <shikharsks28@gmail.com>
2026-06-18 17:17:31 +0530

    Refactor Selection Sort for improved readability

--- Day18/Selection Sort.cpp
+++ Day18/Selection Sort.cpp
@@ -4,16 +4,21 @@ int main(){
     int n, a[100], temp, minIndex;
     cout<<"Enter size: ";
     cin>>n;
-    for(int i=0;i<n;i++) cin>>a[i];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
     for(int i=0;i<n-1;i++){
         minIndex=i;
         for(int j=i+1;j<n;j++){
-            if(a[j]<a[minIndex]) minIndex=j;
+            if(a[j]<a[minIndex])
+                minIndex=j;
         }
-        temp=a[i]; a[i]=a[minIndex]; a[minIndex]=temp;
+        temp=a[i]; 
+        a[i]=a[minIndex]; 
+        a[minIndex]=temp;
     }
     cout<<"Sorted: ";
-    for(int i=0;i<n;i++) cout<<a[i]<<" ";
+    for(int i=0;i<n;i++) 
+        cout<<a[i]<<" ";
     cout<<endl;
     return 0;
 }
