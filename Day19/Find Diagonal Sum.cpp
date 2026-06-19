b7d8a8f5debbd00c51581fb611a60fd2264a492b
Shikhar <shikharsks28@gmail.com>
2026-06-19 20:14:47 +0530

    Update Find Diagonal Sum.cpp

--- Day19/Find Diagonal Sum.cpp
+++ Day19/Find Diagonal Sum.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
 using namespace std;
-int main(){
+int main()
+{
     int n, a[10][10], sum=0;
+    
     cout<<"Enter size of square matrix: ";
     cin>>n;
-    for(int i=0;i<n;i++) for(int j=0;j<n;j++) cin>>a[i][j];
-    for(int i=0;i<n;i++) sum=sum+a[i][i];
+    for(int i=0;i<n;i++) 
+        for(int j=0;j<n;j++) 
+            cin>>a[i][j];
+    for(int i=0;i<n;i++)
+        sum=sum+a[i][i];
     cout<<"Diagonal sum = "<<sum<<endl;
     return 0;
 }
