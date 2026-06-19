40b9f5159e1843a73f357aed4e5f2a119557a081
Shikhar <shikharsks28@gmail.com>
2026-06-19 20:18:29 +0530

    Update Transpose Matrix.cpp

--- Day19/Transpose Matrix.cpp
+++ Day19/Transpose Matrix.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 using namespace std;
+
 int main(){
     int r, c, a[10][10];
+    
     cout<<"Enter rows and columns: ";
     cin>>r>>c;
-    for(int i=0;i<r;i++) for(int j=0;j<c;j++) cin>>a[i][j];
-    cout<<"Transpose:\n";
+    for(int i=0;i<r;i++) 
+        for(int j=0;j<c;j++) 
+            cin>>a[i][j];
+    cout<<"Transpose of matrix:\n";
     for(int i=0;i<c;i++){
-        for(int j=0;j<r;j++) cout<<a[j][i]<<" ";
+        for(int j=0;j<r;j++) 
+            cout<<a[j][i]<<" ";
         cout<<endl;
     }
     return 0;
